436cfd5b4b6897bbbda56b4b349ae5724d432fb7
Lei Hua <leihua2009@gmail.com>
2018-06-09 09:04:40 -0400

    Provider to component

--- src/components/UiComponent.js
+++ src/components/UiComponent.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 
+import { Provider } from 'mobx-react'
+import { UiStore } from './../stores'
+
 class UiComponent extends Component {
   render() {
     return (
-      <div>
-        <h1>UiComponent</h1>
-      </div>
+      <Provider UiStore={UiStore}>
+        <div>
+          <h1>UiComponent</h1>
+        </div>
+      </Provider>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,4 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-import { Provider } from 'mobx-react'
-import { UiStore } from './stores'
-
-ReactDOM.render(
-  <Provider UiStore={UiStore}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
