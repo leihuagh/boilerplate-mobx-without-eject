b8e22956569a0b1d2913554f2587f4c3c4d0f9e7
Lei Hua <leihua2009@gmail.com>
2018-06-09 09:46:15 -0400

    config file es6

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,7 @@
-const { injectBabelPlugin } = require('react-app-rewired')
-const rewireMobX = require('react-app-rewire-mobx')
+import { injectBabelPlugin } from 'react-app-rewired'
+import { rewireMobX } from 'react-app-rewire-mobx'
 
-module.exports = function override(config, env) {
+export default (config, env) => {
   config = injectBabelPlugin('babel-plugin-styled-components', config)
   config = rewireMobX(config, env)
 
--- src/components/uicomponents/Ui.js
+++ src/components/uicomponents/Ui.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
+import { inject, observer } from 'mobx-react'
 
-export default class Ui extends Component {
+@inject('UiStore')
+@observer
+class Ui extends Component {
   render() {
+    const { UiStore } = this.props
+    console.log(UiStore)
     return (
       <div>
         <h1>Ui</h1>
@@ -9,3 +14,5 @@ export default class Ui extends Component {
     )
   }
 }
+
+export default Ui
